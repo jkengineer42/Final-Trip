9b15ca303a8e6d2901f75a3ecc05693cfc9fd85f
42 <jeremie.konda@outlook.fr>
2025-03-31 01:32:53 +0200

    Update Destination.php

--- PHP/Destination.php
+++ PHP/Destination.php
@@ -27,8 +27,10 @@ $searchKeyword = isset($_GET['search']) ? $_GET['search'] : '';
 
 //Filtrer les voyages selon le mot-clé
 $filteredVoyages = array_filter($voyages['voyages'], function($voyage) use ($searchKeyword) {
-	return strpos(strtolower($voyage['titre']), strtolower($searchKeyword)) !== false || strpos(strtolower($voyage['description']), strtolower($searchKeyword)) !== false; 
-	});
+    return strpos(strtolower($voyage['titre']), strtolower($searchKeyword)) !== false || 
+           strpos(strtolower($voyage['description']), strtolower($searchKeyword)) !== false || 
+           strpos(strtolower($voyage['pays']), strtolower($searchKeyword)) !== false;
+});
 
 // Trier les voyages par niveau décroissant
 usort($filteredVoyages, function ($a, $b) {
@@ -141,7 +143,7 @@ $voyagesToShow = array_slice($filteredVoyages, $startIndex, $tripsPerPage);
             // Afficher chaque voyage trié par niveau décroissant
             if (isset($voyagesToShow) && is_array($voyagesToShow)) {
                 foreach ($voyagesToShow as $voyage) {
-                    echo '<a href="details-expedition-polaire.php?id=' . $voyage['id'] . '" class="trip-card-link">';
+                    echo '<a href="voyage_detail.php?id=' . $voyage['id'] . '" class="trip-card-link">';
                     echo '  <article class="trip-card">';
                     echo '    <img src="' . $voyage['image'] . '" alt="' . $voyage['titre'] . '">';
                     echo '    <div class="trip-info">';
@@ -177,20 +179,6 @@ $voyagesToShow = array_slice($filteredVoyages, $startIndex, $tripsPerPage);
             </div>
     </main>
 
-    <footer>
-        <h2>Le dernier voyage que vous rêvez d’avoir</h2>
-        <div class="contact">
-            <p><strong>Adresse :</strong> <a href="#">34, Boulevard Haussmann, Paris 75009</a></p>
-            <p><strong>Numéro :</strong> <a href="tel:0749685456">07 49 68 54 56</a></p>
-            <p><strong>Email :</strong> <a href="mailto:contact@final-trip.com">contact@final-trip.com</a></p>
-        </div>
-        <p class="copyright">© 2025 Final Trip, ALL RIGHTS RESERVED.</p>
-        <hr class="hr2">
-        <div class="links">
-            <a href="#">Mentions légales</a>
-            <a href="#">Politique de confidentialité</a>
-            <a href="#">Conditions d’utilisations</a>
-        </div>
-    </footer>
+    <?php include('footer.php'); ?>
 </body>
 </html>
