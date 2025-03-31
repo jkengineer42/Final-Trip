0f0466706819a2dedeeda9702cc974e3ea36c8be
42 <jeremie.konda@outlook.fr>
2025-03-31 09:35:13 +0200

    Update Profil.php

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -105,16 +105,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="../Css/Profil.css">
 </head>
 <body>
-    <header>
-        <a href="Accueil.php" class="logo">FINAL TRIP</a>
-        <div class="right">
-            <a href="A-propos.php" class="head1">Qui sommes nous ?</a>
-            <a href="Destination.php" class="head1">Destination</a>
-            <button class="encadré">Contact</button>
-            <a href="Profil.php" class="img1"><img src="../assets/icon/User.png"></a>
-            <a href="#" class="img2"><img src="../assets/icon/Shopping cart.png"></a>
-        </div>
-    </header>
+     <?php include('header.php'); ?>
 
     <hr class="hr1">
 
@@ -175,20 +166,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </main>
 
-    <footer>
-        <h2>Le dernier voyage que vous rêvez d’avoir</h2>
-        <div class="contact">
-            <p><strong>Adresse :</strong> <a href="#">34, Boulevard Haussmann, Paris 75009</a></p>
-            <p><strong>Numéro :</strong> <a href="tel:0749685456">07 49 68 54 56</a></p>
-            <p><strong>Email :</strong> <a href="mailto:contact@final-trip.com">contact@final-trip.com</a></p>
-        </div>
-        <p class="copyright">© 2025 Final Trip, ALL RIGHTS RESERVED.</p>
-        <hr class="hr2">
-        <div class="links">
-            <a href="#">Mentions légales</a>
-            <a href="#">Politique de confidentialité</a>
-            <a href="#">Conditions d’utilisation</a>
-        </div>
-    </footer>
+    <footer><?php include('footer.php'); ?></footer>
 </body>
 </html>
