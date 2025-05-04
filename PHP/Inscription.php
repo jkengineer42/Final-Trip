bfc1fa315ef63e1231684865a1fb07318e01b04e
Dmkn78 <dmkn78@gmail.com>
2025-05-04 20:27:17 +0200

    Update Inscription.php

--- PHP/Inscription.php
+++ PHP/Inscription.php
@@ -85,6 +85,8 @@ if (isset($_SESSION['user_email'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription - FINAL TRIP</title>
     <link rel="stylesheet" href="../Css/Inscription.css">
+    <script src="../Javascript/theme.js"></script>
+    <script src="../Javascript/formulaire.js"></script>
 </head>
 <body>
      <?php include('header.php'); ?>
