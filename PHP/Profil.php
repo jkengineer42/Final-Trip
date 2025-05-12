aa853bea2e10147429094eb2ebc763c1d97bf0b6
42 <jeremie.konda@outlook.fr>
2025-05-12 14:21:48 +0200

    Update Profil.php

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -290,6 +290,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     const isAdminEditingAnotherUser = <?= json_encode($isAdmin && isset($_SESSION['user_email']) && isset($editEmail) && $_SESSION['user_email'] !== $editEmail) ?>;
     const adminUpdateDelay = 5000; // Délai en millisecondes (5 secondes)
 </script>
-<script src="../Javascript/Profil.js" defer></script>
+<script src="../Javascript/profil.js" defer></script>
 </body>
 </html>
