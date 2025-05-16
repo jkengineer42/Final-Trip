824301acb2cef34a8dc4ae015cd835a99c519291
42 <jeremie.konda@outlook.fr>
2025-05-16 20:46:36 +0200

    Update footer.php

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <h2>Le dernier voyage que vous rêvez d’avoir</h2>
     <div class="contact">
-        <p><strong>Adresse :</strong> <a href="#">34, Boulevard Haussmann, Paris 75009</a></p>
+        <p><strong>Adresse :</strong> <a href="https://maps.google.com/?q=34+Boulevard+Haussmann+Paris+75009">34, Boulevard Haussmann, Paris 75009</a></p>
         <p><strong>Numéro :</strong> <a href="tel:0749685456">07 49 68 54 56</a></p>
         <p><strong>Email :</strong> <a href="mailto:contact@final-trip.com">contact@final-trip.com</a></p>
     </div>
