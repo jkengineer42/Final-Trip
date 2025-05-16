16203b490d3392beeec6bf9925ea380a18586758
42 <jeremie.konda@outlook.fr>
2025-05-16 21:31:36 +0200

    Update Profil.php

--- PHP/Profil.php
+++ PHP/Profil.php
@@ -148,14 +148,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 <header>
-    <a href="Accueil.php" class="logo">FINAL TRIP</a>
-    <div class="right">
-        <a href="A-propos.php" class="head1">Qui sommes nous ?</a>
-        <a href="Destination.php" class="head1">Destination</a>
-        <button class="encadré">Contact</button>
-        <a href="Profil.php" class="img1"><img src="../assets/icon/User.png" alt="Profil"></a>
-        <a href="#" class="img2"><img src="../assets/icon/Shopping cart.png" alt="Panier"></a>
-    </div>
+    <?php include('header.php'); ?>
 </header>
     <hr class="hr1">
 
