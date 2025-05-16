faa23bad1e9aa73016a8ea4cc5adc156c64dd99c
42 <jeremie.konda@outlook.fr>
2025-05-16 20:18:03 +0200

    Update Accueil.php

--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -15,6 +15,7 @@ if (isset($_SESSION['user_email'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Final Trip</title>
+    <link rel="stylesheet" href="../Css/global.css">
     <link rel="stylesheet" href="../Css/Accueil.css">
     <script src="../Javascript/theme.js"></script>
 </head>
